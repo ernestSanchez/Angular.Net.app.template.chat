c60468eb549d8adb01ccc784e38d5174a9c19e7b
ernestSanchez <ernest.art28@gmail.com>
2020-06-21 21:24:23 +0200

    first steps

---
+++ Angular.Net.Core/Angular.Net.Core/ClientApp/src/app/chat/chat.component.ts
@@ -0,0 +1,22 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'chat-app',
+  templateUrl: './chat.component.html'
+})
+export class ChatComponent {
+  public lstMessages: string[];
+
+  constructor(http: HttpClient, @Inject("BASE_URL") baseUrl: string) {
+    http.get<Message[]>(baseUrl + "api/Chat/Message").subscribe(result => {
+      this.lstMessages = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Message {
+  Id: number,
+  Name: String,
+  Message: string;
+}
---
+++ Angular.Net.Core/Angular.Net.Core/Controllers/ChatController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Angular.Net.Core.Models.ViexModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Angular.Net.Core.Controllers
+{
+    [Route("api/[controller]")]
+    public class ChatController : Controller
+    {
+        private Models.MyDBContext db;
+
+        public ChatController(Models.MyDBContext context)
+        {
+            db = context;
+        }
+        [HttpGet("[action]")]
+        public IEnumerable<MessageViewModel> Message()
+        {
+
+
+        List<MessageViewModel> lst = (from d in db.Message
+                                      select new MessageViewModel
+                                      {
+                                          Id = d.Id,
+                                          Name = d.Name,
+                                          Text = d.Text
+                                      }).ToList();
+        return lst;
+        }
+    }
+}
